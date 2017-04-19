adb6d28a72510f991cd17c56495bafc9a62209bc
xuexiang <yeoncclear@gmail.com>
2017-04-19 13:49:45 +0800

    twosum sub

---
+++ 1.TwoSum.php
@@ -0,0 +1,18 @@
+<?
+	//$arr = [2,7,11,15]; 
+	//$target = 17;
+	//$len = count($arr);
+	
+	function TwoSum($arr,$target,$len){
+		for($i=0;$i<$len;$i++){
+			for($j=$i;$j<$len;$j++){
+				$count = $arr[$j] + $arr[$i];
+				if($count == $target){
+					$resource[] = $i;
+					$resource[] = $j;
+				}
+			}
+		}
+		return $resource;	
+	}
+?>
